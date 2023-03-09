eb3590b7e7cc514d1cc9875ec99902cc72a81b4a
RCherryB <127362808+RCherryB@users.noreply.github.com>
2023-03-08 23:50:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+document.getElementById("listaContacto-click1").onmouseover = function () {
+    document.getElementById("listaContacto-click1").style.color = "#4e031e";
+    document.getElementById("listaContacto-click1").innerHTML = "Sel.Mar@gmail.com";
+}
+document.getElementById("listaContacto-click1").onmouseout = function () {
+    document.getElementById("listaContacto-click1").style.color = "#4e031e";
+    document.getElementById("listaContacto-click1").innerHTML = "Email: Sel.Mar@gmail.com";
+}
+document.getElementById("listaContacto-click2").onmouseover = function () {
+    document.getElementById("listaContacto-click2").style.color = "#4e031e";
+    document.getElementById("listaContacto-click2").innerHTML = "(533) 463-9181";
+}
+document.getElementById("listaContacto-click2").onmouseout = function () {
+    document.getElementById("listaContacto-click2").style.color = "#4e031e";
+    document.getElementById("listaContacto-click2").innerHTML = "Teléfono: (533) 463-9181";
+}
+document.getElementById("listaContacto-click3").onmouseover = function () {
+    document.getElementById("listaContacto-click3").style.color = "#4e031e";
+    document.getElementById("listaContacto-click3").innerHTML = "Perico- Jujuy";
+}
+document.getElementById("listaContacto-click3").onmouseout = function () {
+    document.getElementById("listaContacto-click3").style.color = "#4e031e";
+    document.getElementById("listaContacto-click3").innerHTML = "Localidad: Perico - Jujuy";
+}
+document.getElementById("listaContacto-click4").onmouseover = function () {
+    document.getElementById("listaContacto-click4").style.color = "#4e031e";
+    document.getElementById("listaContacto-click4").innerHTML = "J. B. Alberdi 1600";
+}
+document.getElementById("listaContacto-click4").onmouseout = function () {
+    document.getElementById("listaContacto-click4").style.color = "#4e031e";
+    document.getElementById("listaContacto-click4").innerHTML = "J. B. Alberdi 1600";
+}
+document.getElementById("listaContacto-click5").onmouseover = function () {
+    document.getElementById("listaContacto-click5").style.color = "#4e031e";
+    document.getElementById("listaContacto-click5").innerHTML = "Código postal 4608";
+}
+document.getElementById("listaContacto-click5").onmouseout = function () {
+    document.getElementById("listaContacto-click5").style.color = "#4e031e";
+    document.getElementById("listaContacto-click5").innerHTML = "Código Postal: 4608";
+}
